b29aefbac3f6d1648baaf634f16c1914219a3050
1396954535@qq.com <1396954535@qq.com>
2022-06-14 22:35:04 +0800

    优化代码

--- shiro-client/src/main/java/cn/zflzqy/shiroclient/exception/AuthorizationControllerAdvice.java
+++ shiro-client/src/main/java/cn/zflzqy/shiroclient/exception/AuthorizationControllerAdvice.java
@@ -15,6 +15,15 @@ public class AuthorizationControllerAdvice {
     public AuthorizationControllerAdvice() {
     }
 
+    /**
+    * @description：无权访问的问题
+    * @author: zfl
+    * @return: org.springframework.web.servlet.ModelAndView
+    *  @param request
+     * @param response
+     * @param ex
+    * @time: 2022/6/14 20:45
+     */
     @ExceptionHandler({AuthorizationException.class})
     public ModelAndView resolveAuthorizationException(HttpServletRequest request, HttpServletResponse response, Exception ex) {
         if (StrUtil.contains(request.getContentType(), "application/json")) {
