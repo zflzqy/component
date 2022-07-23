66673fa492bc72f75bba8849fa0365421b466a3b
zflzqy <1396954535@qq.com>
2022-07-23 22:37:59 +0800

    增加通过配置解析数据库字段映射

--- flink/readMysqlBinlog/src/main/java/cn/zflzqy/readMysqlBinlog/StreamingJob.java
+++ flink/readMysqlBinlog/src/main/java/cn/zflzqy/readMysqlBinlog/StreamingJob.java
@@ -18,17 +18,21 @@
 
 package cn.zflzqy.readMysqlBinlog;
 
-import cn.zflzqy.readMysqlBinlog.db.MyAppDataBase;
-import cn.zflzqy.readMysqlBinlog.pool.DruidPool;
+import cn.zflzqy.readMysqlBinlog.db.DataBase;
 import cn.zflzqy.readMysqlBinlog.sink.JdbcTemplateSink;
+import com.alibaba.fastjson2.JSONObject;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -46,6 +50,11 @@ import org.slf4j.LoggerFactory;
 public class StreamingJob {
 	private static final Logger LOGGER = LoggerFactory.getLogger(StreamingJob.class);
 	public static void main(String[] args) throws Exception {
+		ParameterTool configPath = ParameterTool.fromArgs(args);
+//		JSONObject.parseObject(FileUtils.readFile(new File(configPath.get("configPath")),"utf-8"));
+		// 从外部配置读取文件信息 同时可以提供多种方式读取外部配置信息，比如http,数据库，文件，以策略模式搞，同时可以考虑不同模式的合并
+		// 一个binglo表日志，写入到一个或多个库或表中，支持kafka和mysqlbinlog日志两种方式，以工厂模式构建
+		// 增加字段映射示例
 		MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
 				.hostname("192.168.50.102")
 				.port(3306)
@@ -63,18 +72,16 @@ public class StreamingJob {
 		// enable checkpoint
 		env.enableCheckpointing(3000);
 		// 构建数据库连接信息
-		MyAppDataBase myAppDataBase = new MyAppDataBase();
+		DataBase myAppDataBase = new DataBase();
 		myAppDataBase.setIp("192.168.50.102");
 		myAppDataBase.setPort(3306);
 		myAppDataBase.setUsername("root");
 		myAppDataBase.setPassword("123456");
 		myAppDataBase.setDatabaseName("myapp");
 
-		env
-				.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
-				// set 4 parallel source tasks
-				.setParallelism(1)
-				.flatMap(new RichFlatMapFunction<String, Object>() {
+		env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+			.setParallelism(1)
+			.flatMap(new RichFlatMapFunction<String, Object>() {
 					@Override
 					public void flatMap(String s, Collector<Object> collector) throws Exception {
 						// 将得到数据，反射调用方法，传入jdbTemplate和数据源，可以同时提供事务级处理
@@ -82,22 +89,7 @@ public class StreamingJob {
 						collector.collect(s);
 					}
 				})
-				.addSink(new JdbcTemplateSink<>(myAppDataBase))
-				.setParallelism(1); // use parallelism 1 for sink to keep message ordering
-		env
-				.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
-				// set 4 parallel source tasks
-				.setParallelism(1)
-				.flatMap(new RichFlatMapFunction<String, Object>() {
-					@Override
-					public void flatMap(String s, Collector<Object> collector) throws Exception {
-						// 将得到数据，反射调用方法，传入jdbTemplate和数据源，可以同时提供事务级处理
-						LOGGER.info("得到的数据1：{}",s);
-						collector.collect(s);
-					}
-				})
-				.addSink(new JdbcTemplateSink<>(myAppDataBase))
-				.setParallelism(1); // use parallelism 1 for sink to keep message ordering
+			.addSink(new JdbcTemplateSink<>(myAppDataBase));
 
 		env.execute("Print MySQL Snapshot + Binlog");
 	}
