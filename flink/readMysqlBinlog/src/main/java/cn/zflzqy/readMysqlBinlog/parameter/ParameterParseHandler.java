c7622b0266f93997deebec9161911b80579031f9
zflzqy <1396954535@qq.com>
2022-07-24 10:11:15 +0800

    重构参数解析方式，使用策略加工厂解决

---
+++ flink/readMysqlBinlog/src/main/java/cn/zflzqy/readMysqlBinlog/parameter/ParameterParseHandler.java
@@ -0,0 +1,29 @@
+package cn.zflzqy.readMysqlBinlog.parameter;
+
+import com.alibaba.fastjson2.JSONObject;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * @Author: zfl
+ * @Date: 2022-07-24-9:26
+ * @Description: 参数解析
+ */
+public abstract class ParameterParseHandler {
+    protected static final Logger LOGGER = LoggerFactory.getLogger(ParameterParseHandler.class);
+    // 下个处理者
+    private ParameterParseHandler next;
+    //处理请求的方法
+    public abstract JSONObject handleRequest(ParameterTool parameterTool);
+
+    public ParameterParseHandler getNext() {
+        return next;
+    }
+
+    public void setNext(ParameterParseHandler next) {
+        this.next = next;
+    }
+}
