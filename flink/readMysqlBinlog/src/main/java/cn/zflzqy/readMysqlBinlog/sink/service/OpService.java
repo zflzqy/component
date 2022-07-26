171f105b6a8fa78f15f799be63015aecc389989d
zflzqy <1396954535@qq.com>
2022-07-26 21:30:22 +0800

    增加sql语句及参数构建

---
+++ flink/readMysqlBinlog/src/main/java/cn/zflzqy/readMysqlBinlog/sink/service/OpService.java
@@ -0,0 +1,20 @@
+package cn.zflzqy.readMysqlBinlog.sink.service;
+
+import cn.zflzqy.readMysqlBinlog.sink.enums.OpEnum;
+import com.alibaba.fastjson2.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Tuple2;
+
+import java.util.List;
+
+/**
+ * @Author: zfl
+ * @Date: 2022-07-26-19:39
+ * @Description:
+ */
+public interface OpService {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(OpEnum.class);
+    List<Tuple2<String,List<Object>>> doOp(JSONObject data, String idField, String tableName, JSONObject tableMapping);
+}
