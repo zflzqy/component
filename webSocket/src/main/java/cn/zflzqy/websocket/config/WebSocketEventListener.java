18e6f7deeaf39c75f455920ba49e0d9849dd9a9f
zflzqy <1396954535@qq.com>
2022-08-17 21:41:01 +0800

    websocket增加用户监听

--- webSocket/src/main/java/cn/zflzqy/websocket/config/WebSocketEventListener.java
+++ webSocket/src/main/java/cn/zflzqy/websocket/config/WebSocketEventListener.java
@@ -7,19 +7,26 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.security.Principal;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class WebSocketEventListener {
- 
+    private static final ConcurrentHashMap<String, Principal> users = new ConcurrentHashMap<>(8);
     private static final Logger log = LoggerFactory.getLogger(WebSocketEventListener.class);
  
     @EventListener
     public void handleConnectListener(SessionConnectedEvent event) {
         log.info("[ws-connected] socket connect: {}", event.getMessage());
-        // do someting ...
+        users.put(event.getUser().getName(),event.getUser());
     }
  
     @EventListener
     public void handleDisconnectListener(SessionDisconnectEvent event) {
         log.info("[ws-disconnect] socket disconnect: {}", event.getMessage());
-        // do someting ...
+        users.remove(event.getUser().getName());
     }
+    public static  ConcurrentHashMap<String,Principal> getUsers(){
+        return users;
+    }
+
 }
