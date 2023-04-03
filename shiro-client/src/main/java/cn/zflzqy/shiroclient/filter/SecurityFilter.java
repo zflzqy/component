fa00a6f0cab68d5ec64f146fab0d1ef7ffb41419
zflzqy <1396954535@qq.com>
2023-04-03 20:20:55 +0800

    优化代码

--- shiro-client/src/main/java/cn/zflzqy/shiroclient/filter/SecurityFilter.java
+++ shiro-client/src/main/java/cn/zflzqy/shiroclient/filter/SecurityFilter.java
@@ -35,76 +35,79 @@ import java.util.concurrent.TimeUnit;
 public class SecurityFilter extends io.buji.pac4j.filter.SecurityFilter {
     private StringRedisTemplate stringRedisTemplate;
     private ShiroRedisProperties shiroRedisProperties;
-    /** token模式重定向地址*/
+    /**
+     * token模式重定向地址
+     */
     private static String tokenRedirectUrl;
 
-    public SecurityFilter(StringRedisTemplate stringRedisTemplate,ShiroRedisProperties shiroRedisProperties) {
+    public SecurityFilter(StringRedisTemplate stringRedisTemplate, ShiroRedisProperties shiroRedisProperties) {
         this.stringRedisTemplate = stringRedisTemplate;
         this.shiroRedisProperties = shiroRedisProperties;
         this.buildTokenUlr();
     }
 
-    public SecurityFilter(){
+    public SecurityFilter() {
         this.buildTokenUlr();
     }
 
     /**
      * 构建token模式下的重定向url
      */
-    public void buildTokenUlr(){
-        Map<String,String> param = MapUtil.newHashMap(5);
-        if (shiroRedisProperties!=null&&ShiroRedisProperties.TOKEN.equals(shiroRedisProperties.getMode())) {
-            param.put("response_type","code");
-            param.put("client_id",shiroRedisProperties.getClientId());
-            param.put("redirect_uri",StrUtil.removeSuffix(shiroRedisProperties.getCallbackUrl(),"/")+ShiroRedisProperties.CALLBACK_LOGIN_PATH);
-            tokenRedirectUrl = StrUtil.addSuffixIfNot(shiroRedisProperties.getCasUrl() ,"/")+"oauth2.0/authorize?"+ HttpUtil.toParams(param);
+    public void buildTokenUlr() {
+        Map<String, String> param = MapUtil.newHashMap(5);
+        if (shiroRedisProperties != null && ShiroRedisProperties.TOKEN.equals(shiroRedisProperties.getMode())) {
+            param.put("response_type", "code");
+            param.put("client_id", shiroRedisProperties.getClientId());
+            param.put("redirect_uri", StrUtil.removeSuffix(shiroRedisProperties.getCallbackUrl(), "/") + ShiroRedisProperties.CALLBACK_LOGIN_PATH);
+            tokenRedirectUrl = StrUtil.addSuffixIfNot(shiroRedisProperties.getCasUrl(), "/") + "oauth2.0/authorize?" + HttpUtil.toParams(param);
         }
     }
 
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
-        // token模式需要续费accessToken,同时需要校验是否需要登录权限 todo
+        // token模式需要续费accessToken,同时需要校验是否需要登录权限
         if (!ShiroRedisProperties.TOKEN.equals(shiroRedisProperties.getMode())) {
             super.doFilter(servletRequest, servletResponse, filterChain);
+            // 用户会话
+            String session = SecurityUtils.getSubject().getSession().getId().toString();
             // 刷新会话缓存信息
-            if (stringRedisTemplate != null) {
-                // 刷新会话缓存信息
-                String session = SecurityUtils.getSubject().getSession().getId().toString();
-                if (StrUtil.isNotBlank(session)) {
-                    // 获取st
-                    String st = stringRedisTemplate.opsForValue().get(CallbackFilter.SESSION_ST_KEY + session);
-                    if (StrUtil.isNotBlank(st)) {
-                        // 刷新缓存过期时间
-                        stringRedisTemplate.expire(CallbackFilter.SESSION_ST_KEY + session, ShiroConfig.EXPIRE, TimeUnit.SECONDS);
-                        stringRedisTemplate.expire(CallbackFilter.ST_KEY + st, ShiroConfig.EXPIRE, TimeUnit.SECONDS);
-                    }else {
-                        stringRedisTemplate.delete(ShiroConfig.SESSION_KEY+session);
-                    }
-                }
+            if (stringRedisTemplate == null || StrUtil.isNotBlank(session)) {
+                return;
             }
-        }else {
+
+            // 获取st
+            String st = stringRedisTemplate.opsForValue().get(CallbackFilter.SESSION_ST_KEY + session);
+            // 刷新st票据缓存信息
+            if (StrUtil.isNotBlank(st)) {
+                // 刷新缓存过期时间
+                stringRedisTemplate.expire(CallbackFilter.SESSION_ST_KEY + session, ShiroConfig.EXPIRE, TimeUnit.SECONDS);
+                stringRedisTemplate.expire(CallbackFilter.ST_KEY + st, ShiroConfig.EXPIRE, TimeUnit.SECONDS);
+            } else {
+                stringRedisTemplate.delete(ShiroConfig.SESSION_KEY + session);
+            }
+
+        } else {
             // 地址匹配
-            HttpServletRequest request = (HttpServletRequest)servletRequest;
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
             String token = request.getHeader(ShiroSessionManager.AUTHORIZATION);
             HttpServletResponse response = (HttpServletResponse) servletResponse;
             // 获取用户信息
-            String userStr = stringRedisTemplate.opsForValue().get(CallbackFilter.TOKEN+token);
-            if (StrUtil.isBlank(userStr)){
+            String userStr = stringRedisTemplate.opsForValue().get(CallbackFilter.TOKEN + token);
+            if (StrUtil.isBlank(userStr)) {
                 response.sendRedirect(tokenRedirectUrl);
                 return;
             }
 
             // 解析用户
             JSONObject userInfo = JSONUtil.parseObj(userStr);
-            if (userInfo.isEmpty()){
+            if (userInfo.isEmpty()) {
                 response.sendRedirect(tokenRedirectUrl);
                 return;
             }
             // token续期
-            stringRedisTemplate.expire(CallbackFilter.TOKEN+token,userInfo.getJSONObject("accessTokenInfo").getLong("expires_in"),TimeUnit.SECONDS);
+            stringRedisTemplate.expire(CallbackFilter.TOKEN + token, userInfo.getJSONObject("accessTokenInfo").getLong("expires_in"), TimeUnit.SECONDS);
 
-            
         }
     }
 }
