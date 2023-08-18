9a1833316588183b0d17c07adc4cb7938d6f71bb
zflzqy <1396954535@qq.com>
2023-08-18 15:12:07 +0800

    修改模型训练代码

--- ai/src/main/java/test.py
+++ ai/src/main/java/test.py
@@ -1,5 +1,6 @@
 import h2o
 from h2o.estimators import H2ORandomForestEstimator
+from h2o.grid.grid_search import H2OGridSearch
 import pymysql.cursors
 import pandas as pd
 
@@ -16,42 +17,44 @@ connection = pymysql.connect(host='172.168.10.3',
 
 try:
     with connection.cursor() as cursor:
-        # Replace with your SQL query
-        sql = "SELECT	tenant_id,create_by,create_time,`no`,	input,usage_and_dosage,production_operation,operation_description,job_start_time,job_duration,job_introduce,random_job,job_by,`status`,	grow_plan_id FROM	t_production_schedule ORDER BY	create_time DESC "
+        sql = "SELECT tenant_id,create_by,DATE_FORMAT(create_time, '%%Y-%%m-%%d %%H:%%i:%%s') as create_time,`no`,input,usage_and_dosage,production_operation,operation_description, DATE_FORMAT(job_start_time, '%%Y-%%m-%%d') as job_start_time,job_duration,job_introduce,random_job,job_by,`status`,grow_plan_id FROM t_production_schedule ORDER BY create_time DESC"
         cursor.execute(sql)
         results = cursor.fetchall()
         
-        # Convert results to pandas DataFrame
         df = pd.DataFrame(results)
-        
-        # Convert pandas DataFrame to H2OFrame
         hf = h2o.H2OFrame(df)
 finally:
     connection.close()
 
-print("H2OFrame的列：")
-print(hf.columns)
+# 特征工程
+hf['weekday'] = hf['create_time'].dayOfWeek()
+hf['hour'] = hf['create_time'].hour()
+
 hf['operation_description'] = hf['operation_description'].asfactor()
-# 假设我们的目标列是"operation_description"，其余的是特征
+
 features = [col for col in hf.columns if col != 'operation_description']
 target = 'operation_description'
 
-# 切分数据集
 train, test = hf.split_frame(ratios=[0.8])
 
-# 训练随机森林模型
-model = H2ORandomForestEstimator()
-model.train(x=features, y=target, training_frame=train)
+# 调整随机森林的参数
+rf_params = {
+    'ntrees': [100, 150, 200],  # 使用更多的树
+    'max_depth': [30, 40, 50] # 更深的树
+}
+rf_grid = H2OGridSearch(model=H2ORandomForestEstimator(nfolds=5),
+                        grid_id='rf_grid',
+                        hyper_params=rf_params)
 
-# 评估模型
-performance = model.model_performance(test_data=test)
+rf_grid.train(x=features, y=target, training_frame=train)
+
+# 获取准确度最高的模型
+best_rf = rf_grid.get_grid(sort_by='accuracy', decreasing=True)[0]
+
+performance = best_rf.model_performance(test_data=test)
 print(performance)
 
-# 保存模型到文件
-# 保存模型到指定文件名
 model_path = "/opt/machine/model/"
-h2o.save_model(model=model, path=model_path, force=True)
+h2o.save_model(model=best_rf, path=model_path, force=True)
 print(model_path)
-#pojo_path = model.download_pojo(path="/opt/machine/modelpo", get_genmodel_jar=True)
-#print(pojo_path)
 
