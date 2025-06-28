48813eacd6ca780a1c957a8e33a26c2938b896dd
zflzqy <1396954535@qq.com>
2025-06-28 17:09:31 +0800

    Update LogoutFilter.java

--- shiro-client/src/main/java/cn/zflzqy/shiroclient/filter/LogoutFilter.java
+++ shiro-client/src/main/java/cn/zflzqy/shiroclient/filter/LogoutFilter.java
@@ -5,12 +5,18 @@ import cn.zflzqy.shiroclient.config.ShiroConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
+import java.io.StringReader;
 
 
 /***
@@ -28,7 +34,7 @@ public class LogoutFilter extends io.buji.pac4j.filter.LogoutFilter {
         String logoutRequest = servletRequest.getParameter("logoutRequest");
         if (StrUtil.isNotBlank(logoutRequest)){
             // 获取st验证票据并删除
-            String st = StrUtil.sub(logoutRequest,logoutRequest.indexOf("<samlp:SessionIndex>")+20,logoutRequest.indexOf("</samlp:SessionIndex>"));
+            String st = extractSessionIndex(logoutRequest);
             if (StrUtil.isNotBlank(st)&&stringRedisTemplate!=null){
                 // 获取存储的会话
                 String session = stringRedisTemplate.opsForValue().get(CallbackFilter.ST_KEY + st);
@@ -43,6 +49,22 @@ public class LogoutFilter extends io.buji.pac4j.filter.LogoutFilter {
 
         }
         super.doFilter(servletRequest, servletResponse, filterChain);
+    }
+
+    public static String extractSessionIndex(String xmlContent) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true); // 关键：启用命名空间支持
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new InputSource(new StringReader(xmlContent)));
+            NodeList nodeList = doc.getElementsByTagNameNS("urn:oasis:names:tc:SAML:2.0:protocol", "SessionIndex");
+            if (nodeList.getLength() > 0) {
+                return nodeList.item(0).getTextContent();
+            }
+        } catch (Exception e) {
+            log.error("Error extracting SessionIndex from XML: {}", e.getMessage(), e);
+        }
+        return null;
     }
 
     public StringRedisTemplate getStringRedisTemplate() {
